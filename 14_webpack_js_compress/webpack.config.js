2027aa8808e533030da04e77c608044c6f472736
kunming100 <kim_tankunming@outlook.com>
2021-08-18 22:43:25 +0800

    update 14_webpack_js_compress、15_webpack_html_compress

---
+++ 14_webpack_js_compress/webpack.config.js
@@ -0,0 +1,18 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build'),
+  },
+  module: {
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
---
+++ 15_webpack_html_compress /webpack.config.js
@@ -0,0 +1,23 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build'),
+  },
+  module: {},
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        // 移除空格
+        collapseWhitespace: true,
+        // 移除注释
+        removeComments: true,
+      },
+    }),
+  ],
+};
